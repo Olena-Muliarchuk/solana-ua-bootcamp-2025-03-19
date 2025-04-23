e3493c00f889885f1b30ec76e4ce12b501dccbbd
Olena Muliarchuk <olena.muliarchuk.r@gmail.com>
2025-04-23 21:31:58 +0000

    Initial code for favorites(the same in practice-3)

---
+++ practice-4/practice-4-in-rust/favorites/programs/favorites/src/lib.rs
@@ -0,0 +1,84 @@
+use anchor_lang::prelude::*;
+
+declare_id!("AHTQzuPoxEM8AnKorFWxX6DwUbswoPBRuLFHHrQ1FuYM");
+
+// Anchor programs always use
+pub const ANCHOR_DISCRIMINATOR_SIZE: usize = 8;
+
+// What we will put inside the Favorites PDA
+#[account]
+#[derive(InitSpace)]
+pub struct Favorites {
+    pub number: u64,
+
+    #[max_len(50)]
+    pub color: String,
+}
+
+// When people call the set_favorites instruction, they will need to provide the accounts that will
+// be modified. This keeps Solana fast!
+#[derive(Accounts)]
+pub struct SetFavorites<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+
+    #[account(
+        init,
+        payer = user,
+        space = ANCHOR_DISCRIMINATOR_SIZE + Favorites::INIT_SPACE,
+        seeds = [b"favorites", user.key().as_ref()],
+        bump,
+    )]
+    pub favorites: Account<'info, Favorites>,
+
+    pub system_program: Program<'info, System>,
+}
+
+// Accounts struct for getting favorites
+#[derive(Accounts)]
+pub struct GetFavorites<'info> {
+    pub user: Signer<'info>,
+    
+    #[account(
+        seeds = [b"favorites", user.key().as_ref()],
+        bump,
+    )]
+    pub favorites: Account<'info, Favorites>,
+}
+
+// Our Solana program!
+#[program]
+pub mod favorites {
+    use super::*;
+
+    // Our instruction handler! It sets the user's favorite number and color
+    pub fn set_favorites(context: Context<SetFavorites>, number: u64, color: String) -> Result<()> {
+        let user_public_key = context.accounts.user.key();
+        msg!("Greetings from {}", context.program_id);
+        msg!(
+            "User {}'s favorite number is {} and favorite color is: {}",
+            user_public_key,
+            number,
+            color
+        );
+
+        context
+            .accounts
+            .favorites
+            .set_inner(Favorites { number, color });
+        Ok(())
+    }
+
+    // We can also add a get_favorites instruction to get the user's favorite number and color
+    pub fn get_favorites(context: Context<GetFavorites>) -> Result<Favorites> {
+        let favorites = &context.accounts.favorites;
+        let favorites_data = &**favorites; 
+        msg!(
+            "User {}'s favorite number is {} and favorite color is: {}",
+            context.accounts.user.key(),
+            favorites_data.number,
+            favorites_data.color
+        );
+        Ok(favorites_data.clone())
+    }
+}
